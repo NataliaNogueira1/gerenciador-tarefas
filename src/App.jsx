85021cf4279414bfa5e5e1c31fdaea2949650a98
Nainai777 <natalianogueir2006@gmail.com>
2025-05-04 23:00:30 -0300

    Commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { v4 } from "uuid";
+import Title from "./components/Title";
 
 function App() {
-  const [tasks, setTasks] = useState([{
-    id: 1,
-    title: "Estudar programação",
-    description: "15 de julho às 10:00",
-    isCompleted: false,
-  },
-  {
-    id: 2,
-    title: "Estudar inglês",
-    description: "16 de julho às 10:00",
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    title: "Estudar matemática",
-    description: "17 de julho às 10:00",
-    isCompleted: false,
-  }])
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);  
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (storedTasks && storedTasks.length > 0) return;
+  
+    const fetchTasks = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10"
+      );
+      const data = await response.json();
+      // Adiciona descrição vazia (porque os dados da API não têm isso)
+      const enrichedData = data.map(task => ({
+        ...task,
+        description: "",
+      }));
+      setTasks(enrichedData);
+    };
+  
+    fetchTasks();
+  }, []);
+  
+  
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
@@ -40,7 +53,7 @@ function App() {
 
   function onAddTaskSubmit (title, description) {
     const newTask = {
-      id: tasks.length + 1,
+      id: v4(),
       title: title,
       description: description,
       isCompleted: false,
@@ -52,7 +65,7 @@ function App() {
   return(
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de tarefas</h1>
+        <Title>Gerenciador de Tarefas</Title>
         <AddTask onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
       </div>
